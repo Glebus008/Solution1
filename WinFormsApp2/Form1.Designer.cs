5ffc6113b1151963d89dd89fbe84bd281670c3fa
Глеб Усольцев <glebus008@gmail.com>
2026-03-28 00:39:07 +0500

    Rebuild4

--- WinFormsApp2/Form1.Designer.cs
+++ WinFormsApp2/Form1.Designer.cs
@@ -3,10 +3,22 @@
     partial class Form1
     {
         private System.ComponentModel.IContainer components = null;
+
+        // Объявляем все элементы здесь
         private System.Windows.Forms.DataGridView dgvMeasurements;
         private System.Windows.Forms.Button btnAddAndCalculate;
-        private System.Windows.Forms.TextBox txtDate, txtTime, txtInterval, txtLoad, txtS1, txtS2;
-        private System.Windows.Forms.Label lblDate, lblTime, lblInterval, lblLoad, lblS1, lblS2;
+        private System.Windows.Forms.TextBox txtDate;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.TextBox txtInterval;
+        private System.Windows.Forms.TextBox txtLoad;
+        private System.Windows.Forms.TextBox txtS1;
+        private System.Windows.Forms.TextBox txtS2;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblInterval;
+        private System.Windows.Forms.Label lblLoad;
+        private System.Windows.Forms.Label lblS1;
+        private System.Windows.Forms.Label lblS2;
 
         protected override void Dispose(bool disposing)
         {
@@ -16,80 +28,158 @@
 
         private void InitializeComponent()
         {
-            this.dgvMeasurements = new System.Windows.Forms.DataGridView();
-            this.btnAddAndCalculate = new System.Windows.Forms.Button();
-
-            // Инициализация полей ввода
-            this.txtDate = new System.Windows.Forms.TextBox();
-            this.txtTime = new System.Windows.Forms.TextBox();
-            this.txtInterval = new System.Windows.Forms.TextBox();
-            this.txtLoad = new System.Windows.Forms.TextBox();
-            this.txtS1 = new System.Windows.Forms.TextBox();
-            this.txtS2 = new System.Windows.Forms.TextBox();
-
-            this.lblDate = new System.Windows.Forms.Label();
-            this.lblTime = new System.Windows.Forms.Label();
-            this.lblInterval = new System.Windows.Forms.Label();
-            this.lblLoad = new System.Windows.Forms.Label();
-            this.lblS1 = new System.Windows.Forms.Label();
-            this.lblS2 = new System.Windows.Forms.Label();
-
-            ((System.ComponentModel.ISupportInitialize)(this.dgvMeasurements)).BeginInit();
-            this.SuspendLayout();
-
-            // Настройка меток и полей (Слева)
-            int labelX = 15, textX = 15, y = 20, step = 50;
-
-            string[] labels = { "Дата:", "Время:", "Интервал (мин):", "Нагрузка (кН):", "S1 (мм):", "S2 (мм):" };
-            Control[] boxes = { txtDate, txtTime, txtInterval, txtLoad, txtS1, txtS2 };
-            Label[] lbls = { lblDate, lblTime, lblInterval, lblLoad, lblS1, lblS2 };
-
-            for (int i = 0; i < labels.Length; i++)
-            {
-                lbls[i].Text = labels[i];
-                lbls[i].Location = new System.Drawing.Point(labelX, y);
-                lbls[i].AutoSize = true;
-
-                boxes[i].Location = new System.Drawing.Point(textX, y + 20);
-                boxes[i].Size = new System.Drawing.Size(150, 23);
-
-                this.Controls.Add(lbls[i]);
-                this.Controls.Add(boxes[i]);
-                y += step;
-            }
-
-            // Кнопка добавления
-            this.btnAddAndCalculate.Location = new System.Drawing.Point(15, y + 10);
-            this.btnAddAndCalculate.Size = new System.Drawing.Size(150, 45);
-            this.btnAddAndCalculate.Text = "ДОБАВИТЬ В ТАБЛИЦУ";
-            this.btnAddAndCalculate.Click += new System.EventHandler(this.btnAddAndCalculate_Click);
-
-            // Таблица (Справа)
-            this.dgvMeasurements.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvMeasurements.Location = new System.Drawing.Point(180, 12);
-            this.dgvMeasurements.Size = new System.Drawing.Size(1000, 450);
-            this.dgvMeasurements.AllowUserToAddRows = false; // Отключаем пустую строку внизу
-
-            // Колонки
-            this.dgvMeasurements.Columns.Add("Date", "Дата");
-            this.dgvMeasurements.Columns.Add("Time", "Время");
-            this.dgvMeasurements.Columns.Add("Interval", "Интервал");
-            this.dgvMeasurements.Columns.Add("Load", "Нагрузка");
-            this.dgvMeasurements.Columns.Add("S1", "S1");
-            this.dgvMeasurements.Columns.Add("S2", "S2");
-            this.dgvMeasurements.Columns.Add("Savg", "S сред.");
-            this.dgvMeasurements.Columns.Add("DeltaS", "ΔS (прир.)");
-            this.dgvMeasurements.Columns.Add("SumS", "ΣS (сумма)");
-            this.dgvMeasurements.Columns.Add("TotalTime", "Сумм. время");
-
-            // Форма
-            this.ClientSize = new System.Drawing.Size(1200, 480);
-            this.Controls.Add(this.dgvMeasurements);
-            this.Controls.Add(this.btnAddAndCalculate);
-            this.Text = "Ввод данных полевого журнала";
-            ((System.ComponentModel.ISupportInitialize)(this.dgvMeasurements)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            dgvMeasurements = new DataGridView();
+            btnAddAndCalculate = new Button();
+            txtDate = new TextBox();
+            txtTime = new TextBox();
+            txtInterval = new TextBox();
+            txtLoad = new TextBox();
+            txtS1 = new TextBox();
+            txtS2 = new TextBox();
+            lblDate = new Label();
+            lblTime = new Label();
+            lblInterval = new Label();
+            lblLoad = new Label();
+            lblS1 = new Label();
+            lblS2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dgvMeasurements).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvMeasurements
+            // 
+            dgvMeasurements.AllowUserToAddRows = false;
+            dgvMeasurements.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvMeasurements.Location = new Point(185, 12);
+            dgvMeasurements.Name = "dgvMeasurements";
+            dgvMeasurements.Size = new Size(900, 430);
+            dgvMeasurements.TabIndex = 0;
+            // 
+            // btnAddAndCalculate
+            // 
+            btnAddAndCalculate.Location = new Point(15, 340);
+            btnAddAndCalculate.Name = "btnAddAndCalculate";
+            btnAddAndCalculate.Size = new Size(150, 45);
+            btnAddAndCalculate.TabIndex = 1;
+            btnAddAndCalculate.Text = "ДОБАВИТЬ";
+            btnAddAndCalculate.Click += btnAddAndCalculate_Click;
+            // 
+            // txtDate
+            // 
+            txtDate.Location = new Point(15, 40);
+            txtDate.Name = "txtDate";
+            txtDate.Size = new Size(150, 23);
+            txtDate.TabIndex = 2;
+            // 
+            // txtTime
+            // 
+            txtTime.Location = new Point(15, 90);
+            txtTime.Name = "txtTime";
+            txtTime.Size = new Size(150, 23);
+            txtTime.TabIndex = 3;
+            // 
+            // txtInterval
+            // 
+            txtInterval.Location = new Point(15, 140);
+            txtInterval.Name = "txtInterval";
+            txtInterval.Size = new Size(150, 23);
+            txtInterval.TabIndex = 4;
+            // 
+            // txtLoad
+            // 
+            txtLoad.Location = new Point(15, 190);
+            txtLoad.Name = "txtLoad";
+            txtLoad.Size = new Size(150, 23);
+            txtLoad.TabIndex = 5;
+            // 
+            // txtS1
+            // 
+            txtS1.Location = new Point(15, 240);
+            txtS1.Name = "txtS1";
+            txtS1.Size = new Size(150, 23);
+            txtS1.TabIndex = 6;
+            // 
+            // txtS2
+            // 
+            txtS2.Location = new Point(15, 290);
+            txtS2.Name = "txtS2";
+            txtS2.Size = new Size(150, 23);
+            txtS2.TabIndex = 7;
+            // 
+            // lblDate
+            // 
+            lblDate.Location = new Point(15, 20);
+            lblDate.Name = "lblDate";
+            lblDate.Size = new Size(100, 23);
+            lblDate.TabIndex = 8;
+            lblDate.Text = "Дата:";
+            // 
+            // lblTime
+            // 
+            lblTime.Location = new Point(15, 70);
+            lblTime.Name = "lblTime";
+            lblTime.Size = new Size(100, 23);
+            lblTime.TabIndex = 9;
+            lblTime.Text = "Время:";
+            // 
+            // lblInterval
+            // 
+            lblInterval.Location = new Point(15, 120);
+            lblInterval.Name = "lblInterval";
+            lblInterval.Size = new Size(100, 23);
+            lblInterval.TabIndex = 10;
+            lblInterval.Text = "Интервал (мин):";
+            // 
+            // lblLoad
+            // 
+            lblLoad.Location = new Point(15, 170);
+            lblLoad.Name = "lblLoad";
+            lblLoad.Size = new Size(100, 23);
+            lblLoad.TabIndex = 11;
+            lblLoad.Text = "Нагрузка (кН):";
+            // 
+            // lblS1
+            // 
+            lblS1.Location = new Point(15, 220);
+            lblS1.Name = "lblS1";
+            lblS1.Size = new Size(100, 23);
+            lblS1.TabIndex = 12;
+            lblS1.Text = "S1 (мм):";
+            // 
+            // lblS2
+            // 
+            lblS2.Location = new Point(15, 270);
+            lblS2.Name = "lblS2";
+            lblS2.Size = new Size(100, 23);
+            lblS2.TabIndex = 13;
+            lblS2.Text = "S2 (мм):";
+            // 
+            // Form1
+            // 
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            ClientSize = new Size(1100, 460);
+            Controls.Add(dgvMeasurements);
+            Controls.Add(btnAddAndCalculate);
+            Controls.Add(txtDate);
+            Controls.Add(txtTime);
+            Controls.Add(txtInterval);
+            Controls.Add(txtLoad);
+            Controls.Add(txtS1);
+            Controls.Add(txtS2);
+            Controls.Add(lblDate);
+            Controls.Add(lblTime);
+            Controls.Add(lblInterval);
+            Controls.Add(lblLoad);
+            Controls.Add(lblS1);
+            Controls.Add(lblS2);
+            MaximumSize = new Size(1116, 499);
+            MinimumSize = new Size(1116, 499);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Инженерный калькулятор";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvMeasurements).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
     }
 }
